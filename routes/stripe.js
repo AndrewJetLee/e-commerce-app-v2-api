f9d603b0d0536c7ccea17202034ba4e711ac72f7
AndrewJetLee <andrewjetlee777@gmail.com>
2021-12-23 20:00:49 -0600

    Added .env config import for stripe route

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
+
 router.post("/payment", (req, res) => {
-    
     stripe.charges.create({
         source: req.body.tokenId,
         amount: req.body.amount,
