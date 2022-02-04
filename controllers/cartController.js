66b6517339a31bf699a013d3116a371bfea72d1d
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-03 19:43:43 -0600

    Refactored all cart route callbacks into cartController

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -29,5 +29,59 @@ module.exports = {
           } catch (err) {
             res.status(500).json(err);
           }
+    },
+    updateCart: async (req, res) => {
+        if (req.body.productId) {
+            try {
+              const updatedCart = await Cart.findOneAndUpdate({
+                userId: req.params.id,
+                "products.productId": req.body.productId
+              }, {
+                $set: {
+                  "products.$.quantity": req.body.quantity,
+                }
+              });
+              const queryCart = await Cart.findOne({ userId: req.params.id });
+              res.status(200).json({ ...queryCart._doc });
+            } catch (err) {
+              res.status(500).json(err);
+            }
+          } else {
+            try {
+              const updatedCart = await Cart.findOneAndUpdate(
+                { userId: req.params.id },
+                { $set: req.body },
+                { new: true }
+              );
+              const queryCart = await Cart.findOne({ userId: req.params.id });
+              res.status(200).json({ ...queryCart._doc });
+            } catch (err) {
+              res.status(500).json(err);
+            }
+          }
+    },
+    deleteCart: async (req, res) => {
+        try {
+            await Cart.findOneAndDelete({ userId: req.params.id });
+            res.status(200).json("Successfully deleted product");
+          } catch (err) {
+            res.status(500).json(err);
+          }
+    }, 
+    getCart: async (req, res) => {
+        try {
+            const cart = await Cart.findOne({ userId: req.params.id });
+            res.status(200).json(cart);
+          } catch (err) {
+            res.status(500).json(err);
+          }
+    },
+    getAllCart: async (req, res) => {
+        try {
+            const carts = await Cart.find();
+            res.status(200).json(carts);
+          } catch (err) {
+            res.status(500).json(err);
+          }
     }
 }
--- routes/cart.js
+++ routes/cart.js
@@ -5,71 +5,21 @@ const {
   verifyTokenAndAdmin,
 } = require("../middlewares/verify");
 const Cart = require("../models/Cart");
-const { addToCart } = require("../controllers/cartController");
+const { addToCart, updateCart, deleteCart, getCart, getAllCart } = require("../controllers/cartController");
 
 //CREATE
 router.post("/", verifyToken, addToCart);
 
 // UPDATE
-router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  if (req.body.productId) {
-    try {
-      const updatedCart = await Cart.findOneAndUpdate({
-        userId: req.params.id,
-        "products.productId": req.body.productId
-      }, {
-        $set: {
-          "products.$.quantity": req.body.quantity,
-        }
-      });
-      const queryCart = await Cart.findOne({ userId: req.params.id });
-      res.status(200).json({ ...queryCart._doc });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  } else {
-    try {
-      const updatedCart = await Cart.findOneAndUpdate(
-        { userId: req.params.id },
-        { $set: req.body },
-        { new: true }
-      );
-      const queryCart = await Cart.findOne({ userId: req.params.id });
-      res.status(200).json({ ...queryCart._doc });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  }
-});
+router.put("/:id", verifyTokenAndAuthorization, updateCart);
 
 // DELETE
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    await Cart.findOneAndDelete({ userId: req.params.id });
-    res.status(200).json("Successfully deleted product");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.delete("/:id", verifyTokenAndAuthorization, deleteCart);
 
 //GET USER CART
-router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    const cart = await Cart.findOne({ userId: req.params.id });
-    res.status(200).json(cart);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/find/:id", verifyTokenAndAuthorization, getCart);
 
 // //GET ALL
-router.get("/", verifyTokenAndAdmin, async (req, res) => {
-  try {
-    const carts = await Cart.find();
-    res.status(200).json(carts);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/", verifyTokenAndAdmin, getAllCart);
 
 module.exports = router;
