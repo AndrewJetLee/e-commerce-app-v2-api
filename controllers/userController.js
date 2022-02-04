d0fc3ac445267d0d4aef7ad453c300fe4e5162a9
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-03 20:28:04 -0600

    auth routes now use userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,52 +1,104 @@
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-    updateUser: async (req, res) => {
-        if (req.body.password) {
-            req.body.password = CryptoJS.AES.encrypt(
-              req.body.password,
-              process.env.PASS_SEC
-            ).toString();
-          }
-          try {
-            const updatedUser = await User.findOneAndUpdate(
-              { id: req.params.id },
-              { $set: req.body },
-              { new: true }
-            );
-        
-            res.status(200).json(updatedUser);
-          } catch (err) {
-            res.status(500).json(err);
-          }
-    },
-    deleteUser: async (req, res) => {
-        try {
-            await User.findByIdAndDelete(req.params.id);
-            res.status(200).json("Successfully deleted user");
-          } catch (err) {
-            res.status(500).json(err);
-          }
-    },
-    getUser: async (req, res) => {
-        try {
-            const user = await User.findById(req.params.id);
-            const { password, ...others } = user._doc;
-            res.status(200).json(others);
-          } catch (err) {
-            res.status(500).json(err);
-          }
-    },
-    getUsers: async (req, res) => {
-        const query = req.query.new;
-        try {
-          const users = query
-            ? await User.find().sort({ _id: -1 }).limit(5)
-            : await User.find();
-          res.status(200).json(users);
-        } catch (err) {
-          res.status(500).json(err);
-        }
+  updateUser: async (req, res) => {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString();
     }
-}
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { id: req.params.id },
+        { $set: req.body },
+        { new: true }
+      );
+
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  deleteUser: async (req, res) => {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Successfully deleted user");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  getUser: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      const { password, ...others } = user._doc;
+      res.status(200).json(others);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  getUsers: async (req, res) => {
+    const query = req.query.new;
+    try {
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(5)
+        : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  registerUser: (req, res) => {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString(),
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+    newUser
+      .save()
+      .then((data) => {
+        console.log("successfully saved user:", data);
+        const { password, ...others } = data._doc;
+        res.status(201).json(others);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  loginUser: async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("User doesn't exist");
+        const hashedPassword = CryptoJS.AES.decrypt(
+          user.password,
+          process.env.PASS_SEC
+        );
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        originalPassword !== req.body.password &&
+          res.status(401).json("Invalid password");
+    
+        const accessToken = jwt.sign(
+          {
+            id: user._id,
+            isAdmin: user.isAdmin,
+          },
+          process.env.JWT_SEC,
+          { expiresIn: "3d" }
+        );
+    
+        const { password, ...others } = user._doc;
+    
+        res.status(200).json({...others, accessToken});
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,62 +1,10 @@
 const router = require("express").Router();
-const User = require("../models/User");
-const CryptoJS = require("crypto-js");
-const jwt = require("jsonwebtoken");
+const { registerUser, loginUser } = require("../controllers/userController");
 
 //REGISTER
-router.post("/register", (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASS_SEC
-    ).toString(),
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-  });
-  newUser
-    .save()
-    .then((data) => {
-      console.log("successfully saved user:", data);
-      const { password, ...others } = data._doc;
-      res.status(201).json(others);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
+router.post("/register", registerUser);
 
 //LOGIN
-router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.body.username });
-    !user && res.status(401).json("User doesn't exist");
-    const hashedPassword = CryptoJS.AES.decrypt(
-      user.password,
-      process.env.PASS_SEC
-    );
-    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-    originalPassword !== req.body.password &&
-      res.status(401).json("Invalid password");
-
-    const accessToken = jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.JWT_SEC,
-      { expiresIn: "3d" }
-    );
-
-    const { password, ...others } = user._doc;
-
-    res.status(200).json({...others, accessToken});
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.post("/login", loginUser);
 
 module.exports = router;
